e7605b2cd803a632bc42199e2a550c745c6d6ea0
israel <israel.cadorin@catolicasc.org.br>
2019-03-28 02:50:46 -0300

    Equacao2Grau

--- Equacao2Grau.java
+++ Equacao2Grau.java
@@ -4,6 +4,9 @@ public class Equacao2Grau {
     private Double valorC;
     private Double delta;
     private Double x1;
+    private Double x2;
+    private Double xVertice;
+    private Double yVertice;
 
     public Equacao2Grau(Double valorA, Double valorB, Double valorC) {
         this.valorA = valorA;
@@ -12,39 +15,68 @@ public class Equacao2Grau {
     }
 
     public Double calcularDelta() {
-        return Math.pow(this.valorB, 2) - 4 * this.valorA * this.valorC;
+        delta = Math.pow(this.valorB, 2) - 4 * this.valorA * this.valorC;
+        return delta;
     }
 
-    public boolean verificar() {
+    public void verificar() {
         if (this.delta < 0) {
             System.out.println("A equação não possui raiz real");
-            return false;
         } else {
-            System.out.println("Calcular duas raízes");
-            return true;
+            valorDelta();
         }
     }
 
-    public Double umaRaiz(){
+    public void valorDelta(){
         if(this.delta == 0){
-            Double x1 = -this.valorB /(2 * this.valorA);
+            x1 = -this.valorB /(2 * this.valorA);
+        } else {
+            calcularX1();
+            calcularX2();
         }
-        return x1;
     }
 
     public Double calcularX1() {
-        return (-this.valorB + Math.sqrt(delta)) / (2 * this.valorA);
+        x1 = (-this.valorB - Math.sqrt(this.delta)) / (2 * this.valorA);
+        return x1;
     }
 
     public Double calcularX2() {
-        return (-this.valorB - Math.sqrt(delta)) / (2 * this.valorA);
+        x2 = (-this.valorB + Math.sqrt(this.delta)) / (2 * this.valorA);
+        return x2;
     }
 
-    public Double calcularXv() {
-        return -(this.valorB / 2 * this.valorA);
+    public Double calcularXVertice() {
+        xVertice = -(this.valorB / 2 * this.valorA);
+        return xVertice;
     }
 
-    public Double calcularYv() {
-        return -(this.delta / 4 * this.valorA);
+    public Double calcularYVertice() {
+        yVertice = -(this.delta / 4 * this.valorA);
+        return yVertice;
+    }
+
+    public String imagem() {
+        if(this.valorA > 0) {
+            return ("[-" + this.valorA + ", +∞)");
+        } else {
+            return ("(-∞, +" + this.valorA + "]");
+        }
+    }
+    @Override
+    public String toString(){
+        return ("Delta: " + this.delta +
+                "\nX1: " + this.x1 +
+                "\nX2: " + this.x2 +
+                "\nX Vértice: " + this.xVertice +
+                "\nY Vértice: " + this.yVertice +
+                "\nConjunto imagem: ");
+    }
+
+    public void geral() {
+        calcularDelta();
+        verificar();
+        calcularXVertice();
+        calcularYVertice();
     }
 }
--- TesteEquacao2Grau.java
+++ TesteEquacao2Grau.java
@@ -1,6 +1,3 @@
-import javax.swing.JOptionPane;
-import java.text.DecimalFormat;
-
 public class TesteEquacao2Grau {
 
     public static void main(String[] args) {
@@ -8,19 +5,12 @@ public class TesteEquacao2Grau {
         Double valorA = Double.valueOf(args[0]);
         Double valorB = Double.valueOf(args[1]);
         Double valorC = Double.valueOf(args[2]);
-        DecimalFormat df = new DecimalFormat("#0.0");
-        //String decision;
-        //decision = JOptionPane.showInputDialog("Would you like to do another operation? Choice Y(yes) or N(no): ");
 
         Equacao2Grau equacao = new Equacao2Grau(valorA, valorB, valorC);
 
-        System.out.println(equacao.calcularDelta());
-        System.out.println(equacao.calcularX1());
-        System.out.println(equacao.calcularX2());
-        System.out.println();
-        System.out.println(equacao.umaRaiz());
-
-
+        equacao.geral();
+        System.out.print(equacao.toString());
+        System.out.println(equacao.imagem());
 
     }
 }
